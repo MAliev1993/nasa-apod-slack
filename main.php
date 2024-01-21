4281fb5f9c07b92f9fc6c5041c2523cbd8c81e0d
maliev <maliev@semcosoft.com>
2024-01-21 20:21:15 +0100

    Fixes

--- main.php
+++ main.php
@@ -1,13 +1,23 @@
 <?php
 
-const GET = 'GET';
-const POST = 'POST';
+require_once('vendor/autoload.php');
 
 use GuzzleHttp\Client as Client;
 
+const GET = 'GET';
+const POST = 'POST';
+
 $nasaApiKey = 'iM1tau6c6NlcqwczM7ng4PECq9CO9m2MDeBe3Sct';
 $nasaUri = 'https://api.nasa.gov/planetary/apod';
-$nasaHttpClient = new Client();
+$nasaHeaders = [
+    "Content-Type" => "application/json",
+    "Accept" => "application/json",
+];
+$nasaHttpClient = new Client([
+    'headers' => $nasaHeaders,
+]);
+
+$nasaRequestOptions = [];
 $nasaResponse = $nasaHttpClient->get($nasaUri, ['query' => 'api_key=' . $nasaApiKey]);
 
 $slackWebhookUri = 'https://hooks.slack.com/services/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';
@@ -18,5 +28,5 @@ $slackHttpClient = new Client();
 $slackResponse = $slackHttpClient->request(
     POST,
     $slackWebhookUri,
-    ['body' => $payload]
+    ['json' => $payload]
 );
